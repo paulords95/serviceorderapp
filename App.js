c114d1361dd3442653ccdaf43763a96b8d6282be
paulords95 <paulords95@gmail.com>
2020-11-25 10:59:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
+
+
+import CodeScanner from './componets/barcode/CodeScanner'
+import logo from './assets/logo.png'
 
 export default function App() {
+ let [fontsLoaded] = useFonts({
+   'Montserrat': require('./assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+ });
+
+ if (!fontsLoaded) {
+   return <AppLoading />;
+ } else {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Image source={logo} style={styles.logo}></Image>
+      <Text style={styles.title}>Registro de Ordens de Serviço de Manutenção</Text>
       <StatusBar style="auto" />
+      <Button title='Ler código de barras' onPress={()=> {
+        return(
+<CodeScanner/>
+        )
+      }}></Button>
+      
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +38,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 80
+
   },
+  logo: {
+    flex: 1,
+    position: "absolute",
+    top:  100,
+    
+  },
+  title: {
+    position: 'relative',
+    fontSize: 25,
+    paddingTop: 130,
+    maxWidth: 250,
+    textAlign: "center",
+  fontFamily: 'Montserrat',
+    fontWeight: "bold"
+  }
 });
